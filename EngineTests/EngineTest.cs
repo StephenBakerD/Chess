39b514f96ce5bccd7bb5ce54e5220800473e7df1
StephenDBaker <stephenbaka@gmail.com>
2021-01-06 20:39:26 -0700

    ...

--- EngineTests/EngineTest.cs
+++ EngineTests/EngineTest.cs
@@ -1,8 +1,28 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 
 namespace EngineTests
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void Create_Game()
+        {
+            //Arrange
+            var game = new Game();
+
+            //Act
+
+            //Assert
+            Assert.True(true);
+        }
+    }
+
+    public class Game
+    {
+        public Game()
+        {
+        }
     }
 }
