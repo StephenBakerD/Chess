cda9d4830e3c9f05987d471856481ac5d5d7f4f1
StephenDBaker <stephenbaka@gmail.com>
2021-01-06 19:59:08 -0700

    Initial check-in

---
+++ Web.Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+
+namespace Chess.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Can_Create_Game()
+        {
+            //Assert
+            Game game = new Game();
+
+            //Arrange
+
+            //Act
+        }
+    }
+}
