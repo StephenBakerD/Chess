eb762266878f005b90030e858bd5535b6d76fefe
StephenDBaker <stephenbaka@gmail.com>
2021-01-06 23:19:10 -0700

    Moved Pieces and Squares into their own files.

---
+++ Engine/Pieces.cs
@@ -0,0 +1,13 @@
+﻿namespace Engine
+{
+    public class Pieces
+    {
+        public const string King = "King";
+        public const string Queen = "Queen";
+        public const string Rook = "Rook";
+        public const string Knight = "Knight";
+        public const string Bishop = "Bishop";
+        public const string Pawn = "Pawn";
+        public const string Empty = "";
+    }
+}
