65d21a2ef8b9eb33bddd6dd5c3efd3a292ce4493
StephenDBaker <stephenbaka@gmail.com>
2021-01-23 17:40:31 -0700

    Board now takes a Dictionary<string, IPiece> as a constructor parameter. Board now contains a static property to retrieve a default Dictionary<string, IPiece> collection.

--- Engine/Board.cs
+++ Engine/Board.cs
@@ -8,87 +8,12 @@ namespace Engine
     {
         private Dictionary<string, IPiece> squares = new Dictionary<string, IPiece>();
 
-        public Board()
+        public Board(Dictionary<string, IPiece> pieces)
         {
-            //Row 1
-            squares[Squares.A1] = new Rook();
-            squares[Squares.B1] = new Knight();
-            squares[Squares.C1] = new Bishop();
-            squares[Squares.D1] = new Queen();
-            squares[Squares.E1] = new King();
-            squares[Squares.F1] = new Bishop();
-            squares[Squares.G1] = new Knight();
-            squares[Squares.H1] = new Rook();
-
-            //Row 2
-            squares[Squares.A2] = new Pawn();
-            squares[Squares.B2] = new Pawn();
-            squares[Squares.C2] = new Pawn();
-            squares[Squares.D2] = new Pawn();
-            squares[Squares.E2] = new Pawn();
-            squares[Squares.F2] = new Pawn();
-            squares[Squares.G2] = new Pawn();
-            squares[Squares.H2] = new Pawn();
-
-            //Row 3
-            squares[Squares.A3] = null;
-            squares[Squares.B3] = null;
-            squares[Squares.C3] = null;
-            squares[Squares.D3] = null;
-            squares[Squares.E3] = null;
-            squares[Squares.F3] = null;
-            squares[Squares.G3] = null;
-            squares[Squares.H3] = null;
-
-            //Row 4
-            squares[Squares.A4] = null;
-            squares[Squares.B4] = null;
-            squares[Squares.C4] = null;
-            squares[Squares.D4] = null;
-            squares[Squares.E4] = null;
-            squares[Squares.F4] = null;
-            squares[Squares.G4] = null;
-            squares[Squares.H4] = null;
-
-            //Row 5
-            squares[Squares.A5] = null;
-            squares[Squares.B5] = null;
-            squares[Squares.C5] = null;
-            squares[Squares.D5] = null;
-            squares[Squares.E5] = null;
-            squares[Squares.F5] = null;
-            squares[Squares.G5] = null;
-            squares[Squares.H5] = null;
-
-            //Row 6
-            squares[Squares.A6] = null;
-            squares[Squares.B6] = null;
-            squares[Squares.C6] = null;
-            squares[Squares.D6] = null;
-            squares[Squares.E6] = null;
-            squares[Squares.F6] = null;
-            squares[Squares.G6] = null;
-            squares[Squares.H6] = null;
-
-            //Row 7
-            squares[Squares.A7] = new Pawn();
-            squares[Squares.B7] = new Pawn();
-            squares[Squares.C7] = new Pawn();
-            squares[Squares.D7] = new Pawn();
-            squares[Squares.E7] = new Pawn();
-            squares[Squares.F7] = new Pawn();
-            squares[Squares.G7] = new Pawn();
-            squares[Squares.H7] = new Pawn();
-
-            //Row 8
-            squares[Squares.A8] = new Rook();
-            squares[Squares.B8] = new Knight();
-            squares[Squares.C8] = new Bishop();
-            squares[Squares.D8] = new Queen();
-            squares[Squares.E8] = new King();
-            squares[Squares.F8] = new Bishop();
-            squares[Squares.G8] = new Knight();
-            squares[Squares.H8] = new Rook();
+            foreach (var piece in pieces)
+            {
+                squares[piece.Key] = piece.Value;
+            }
         }
 
         public IPiece GetSquare(string square) => squares[square];
@@ -104,6 +29,87 @@ namespace Engine
             }
 
             return success;
+        }
+
+        public static Dictionary<string, IPiece> NormalPieceArrangement
+        {
+            get
+            {
+                return new Dictionary<string, IPiece>()
+                {
+                    { Squares.A1, new Rook() },
+                    { Squares.B1, new Knight() },
+                    { Squares.C1, new Bishop() },
+                    { Squares.D1, new Queen() },
+                    { Squares.E1, new King() },
+                    { Squares.F1, new Bishop() },
+                    { Squares.G1, new Knight() },
+                    { Squares.H1, new Rook() },
+
+                    { Squares.A2, new Pawn() },
+                    { Squares.B2, new Pawn() },
+                    { Squares.C2, new Pawn() },
+                    { Squares.D2, new Pawn() },
+                    { Squares.E2, new Pawn() },
+                    { Squares.F2, new Pawn() },
+                    { Squares.G2, new Pawn() },
+                    { Squares.H2, new Pawn() },
+
+                    { Squares.A3, null },
+                    { Squares.B3, null },
+                    { Squares.C3, null },
+                    { Squares.D3, null },
+                    { Squares.E3, null },
+                    { Squares.F3, null },
+                    { Squares.G3, null },
+                    { Squares.H3, null },
+
+                    { Squares.A4, null },
+                    { Squares.B4, null },
+                    { Squares.C4, null },
+                    { Squares.D4, null },
+                    { Squares.E4, null },
+                    { Squares.F4, null },
+                    { Squares.G4, null },
+                    { Squares.H4, null },
+
+                    { Squares.A5, null },
+                    { Squares.B5, null },
+                    { Squares.C5, null },
+                    { Squares.D5, null },
+                    { Squares.E5, null },
+                    { Squares.F5, null },
+                    { Squares.G5, null },
+                    { Squares.H5, null },
+
+                    { Squares.A6, null },
+                    { Squares.B6, null },
+                    { Squares.C6, null },
+                    { Squares.D6, null },
+                    { Squares.E6, null },
+                    { Squares.F6, null },
+                    { Squares.G6, null },
+                    { Squares.H6, null },
+
+                    { Squares.A7, new Pawn()},
+                    { Squares.B7, new Pawn()},
+                    { Squares.C7, new Pawn()},
+                    { Squares.D7, new Pawn()},
+                    { Squares.E7, new Pawn()},
+                    { Squares.F7, new Pawn()},
+                    { Squares.G7, new Pawn()},
+                    { Squares.H7, new Pawn()},
+
+                    { Squares.A8, new Rook()},
+                    { Squares.B8, new Knight()},
+                    { Squares.C8, new Bishop()},
+                    { Squares.D8, new Queen()},
+                    { Squares.E8, new King()},
+                    { Squares.F8, new Bishop()},
+                    { Squares.G8, new Knight()},
+                    { Squares.H8, new Rook() }
+                };
+            }
         }
     }
 }
--- EngineTest/BoardTest.cs
+++ EngineTest/BoardTest.cs
@@ -2,93 +2,16 @@ using Engine;
 using Engine.Pieces;
 using Xunit;
 using System;
+using System.Collections.Generic;
 
 namespace EngineTest
 {
     public class BoardTest
     {
-        private Board board;
-
-        public BoardTest()
+        [Fact]
+        public void Can_Create_Normal_Board()
         {
-            board = new Board();
-        }
-
-        [Theory]
-        [InlineData(Squares.A1, Rook.Name)]
-        [InlineData(Squares.B1, Knight.Name)]
-        [InlineData(Squares.C1, Bishop.Name)]
-        [InlineData(Squares.D1, Queen.Name)]
-        [InlineData(Squares.E1, King.Name)]
-        [InlineData(Squares.F1, Bishop.Name)]
-        [InlineData(Squares.G1, Knight.Name)]
-        [InlineData(Squares.H1, Rook.Name)]
-
-        [InlineData(Squares.A2, Pawn.Name)]
-        [InlineData(Squares.B2, Pawn.Name)]
-        [InlineData(Squares.C2, Pawn.Name)]
-        [InlineData(Squares.D2, Pawn.Name)]
-        [InlineData(Squares.E2, Pawn.Name)]
-        [InlineData(Squares.F2, Pawn.Name)]
-        [InlineData(Squares.G2, Pawn.Name)]
-        [InlineData(Squares.H2, Pawn.Name)]
-
-        [InlineData(Squares.A3, null)]
-        [InlineData(Squares.B3, null)]
-        [InlineData(Squares.C3, null)]
-        [InlineData(Squares.D3, null)]
-        [InlineData(Squares.E3, null)]
-        [InlineData(Squares.F3, null)]
-        [InlineData(Squares.G3, null)]
-        [InlineData(Squares.H3, null)]
-
-        [InlineData(Squares.A4, null)]
-        [InlineData(Squares.B4, null)]
-        [InlineData(Squares.C4, null)]
-        [InlineData(Squares.D4, null)]
-        [InlineData(Squares.E4, null)]
-        [InlineData(Squares.F4, null)]
-        [InlineData(Squares.G4, null)]
-        [InlineData(Squares.H4, null)]
-
-        [InlineData(Squares.A5, null)]
-        [InlineData(Squares.B5, null)]
-        [InlineData(Squares.C5, null)]
-        [InlineData(Squares.D5, null)]
-        [InlineData(Squares.E5, null)]
-        [InlineData(Squares.F5, null)]
-        [InlineData(Squares.G5, null)]
-        [InlineData(Squares.H5, null)]
-
-        [InlineData(Squares.A6, null)]
-        [InlineData(Squares.B6, null)]
-        [InlineData(Squares.C6, null)]
-        [InlineData(Squares.D6, null)]
-        [InlineData(Squares.E6, null)]
-        [InlineData(Squares.F6, null)]
-        [InlineData(Squares.G6, null)]
-        [InlineData(Squares.H6, null)]
-
-        [InlineData(Squares.A7, Pawn.Name)]
-        [InlineData(Squares.B7, Pawn.Name)]
-        [InlineData(Squares.C7, Pawn.Name)]
-        [InlineData(Squares.D7, Pawn.Name)]
-        [InlineData(Squares.E7, Pawn.Name)]
-        [InlineData(Squares.F7, Pawn.Name)]
-        [InlineData(Squares.G7, Pawn.Name)]
-        [InlineData(Squares.H7, Pawn.Name)]
-
-        [InlineData(Squares.A8, Rook.Name)]
-        [InlineData(Squares.B8, Knight.Name)]
-        [InlineData(Squares.C8, Bishop.Name)]
-        [InlineData(Squares.D8, Queen.Name)]
-        [InlineData(Squares.E8, King.Name)]
-        [InlineData(Squares.F8, Bishop.Name)]
-        [InlineData(Squares.G8, Knight.Name)]
-        [InlineData(Squares.H8, Rook.Name)]
-        public void Can_Create_Board(string square, string piece)
-        {
-            Assert.Equal(piece, board.GetSquare(square)?.GetName());
+            var board = new Board(Board.NormalPieceArrangement);
         }
 
         [Theory]
@@ -101,9 +24,11 @@ namespace EngineTest
         [InlineData(Bishop.Name, Squares.C1, Squares.H6)]
         public void Can_Move_Piece(string pieceName, string squareFrom, string squareTo)
         {
+            var board = new Board(Board.NormalPieceArrangement);
+
             //Move the piece
             Assert.True(board.TryMovePiece(squareFrom, squareTo));
-            
+
             //Make sure target square has the knight now, and original square is null
             Assert.Null(board.GetSquare(squareFrom));
             Assert.Equal(pieceName, board.GetSquare(squareTo).GetName());
@@ -115,6 +40,8 @@ namespace EngineTest
         [InlineData(Bishop.Name, Squares.C1, Squares.E4)]
         public void Cannot_Move_Piece_To_Illegal_Square(string pieceName, string squareFrom, string squareTo)
         {
+            var board = new Board(Board.NormalPieceArrangement);
+
             Assert.False(board.TryMovePiece(squareFrom, squareTo));
             Assert.Equal(pieceName, board.GetSquare(squareFrom).GetName());
         }
